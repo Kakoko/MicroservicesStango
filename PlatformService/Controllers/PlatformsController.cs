dc9b362e9158c72fbf188325f6972619de16aa58
Simuzeche Kaluwa <simuzechek@gmail.com>
2025-03-16 00:40:10 +0200

    Feat : added Platforms Controller

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _platform;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo platform, IMapper mapper)
+        {
+            _platform = platform;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetAllPlatforms()
+        {
+            var platforms = _platform.GetAllPlatforms();
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var platform = _platform.GetPlatformById(id);
+            if (platform != null)
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(platform));
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platform = _mapper.Map<Platform>(platformCreateDto);
+            _platform.CreatePlatform(platform);
+            _platform.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platform);
+
+            return CreatedAtAction(
+                    nameof(GetPlatformById),
+                    new { id = platform.Id }, 
+                    platformReadDto           
+             );
+        }
+
+
+
+
+
+
+    }
+}
